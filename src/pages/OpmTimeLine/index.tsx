1abfb6b973e3189b6e859c19e9f807c39c3bb0ba
Paul Gung <gongshaoxu@gmail.com>
2024-03-26 15:47:14 +0800

    update

--- src/pages/OpmTimeLine/index.tsx
+++ src/pages/OpmTimeLine/index.tsx
@@ -1,29 +1,17 @@
 import React from "react";
 
-function renderJumpLinks(str: string) {
-  // 匹配字符串中的 [跳转](地址) 格式
-  const regex = /\[跳转\]\((.*?)\)/g;
-  return str.split(regex).map((part, index) => {
-    if (index % 2 === 0) {
-      // 文本部分
-      return <span key={index}>{part}</span>;
-    } else {
-      // 链接部分
-      const url = part.substring(part.indexOf("(") + 1, part.indexOf(")"));
-      return (
-        <a key={index} href={url} target="_blank" rel="noopener noreferrer">
-          {part}
-        </a>
-      );
-    }
-  });
+function TextWithLinks(text: string) {
+  const parsedText = text.replace(
+    /\[([^\]]+)\]\(([^)]+)\)/g,
+    '<a href="$2">$1</a>'
+  );
+
+  // 使用dangerouslySetInnerHTML将解析后的文本作为 HTML 渲染
+  return <div dangerouslySetInnerHTML={{ __html: parsedText }} />;
 }
 
-function OpmTimeLine() {
-  const str = "这是一段包含[跳转](https://example.com)的文字。";
-  const renderedContent = renderJumpLinks(str);
+const str = "这是一段包含[xxxx](https://example.com)的文字。";
 
-  return <div>{renderedContent}</div>;
-}
+const App = () => <div>{TextWithLinks(str)}</div>;
 
-export default OpmTimeLine;
+export default App;
